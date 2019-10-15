dd89ce5adf154bca592cdebaa86dee4b6269bc73
LIJIAN768 <LIJIAN768@pingan.com.cn>
2019-10-15 22:02:52 +0800

    【MP-In-Action】：基本CRUD

--- mp02/src/test/java/com/mp/test/TestMp.java
+++ mp02/src/test/java/com/mp/test/TestMp.java
@@ -1,5 +1,10 @@
 package com.mp.test;
 
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.segments.MergeSegments;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.mp.beans.Employee;
 import com.mp.mapper.EmployeeMapper;
 import org.junit.Test;
@@ -8,6 +13,10 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author: li_jian
@@ -20,6 +29,85 @@ public class TestMp {
             ClassPathXmlApplicationContext("applicationContext.xml");
 
     private EmployeeMapper employeeMapper = ioc.getBean("employeeMapper", EmployeeMapper.class);
+
+    /**
+     * 通用删除操作
+     */
+    @Test
+    public void testCommonDelete() {
+//        // 1、根据id删除
+//        Integer result = employeeMapper.deleteById(15);
+//        System.out.println("result：" + result);
+//
+//        // 2、根据 deleteByMap 条件删除
+//        Map<String, Object> columnMap  = new HashMap<>();
+//        columnMap.put("last_name","MP");
+//        columnMap.put("email","mybatis-plus@github.com");
+//        Integer ret = employeeMapper.deleteByMap(columnMap);
+//        System.out.println("ret：" + ret);
+
+        // 3、批量删除
+        List<Integer> idList = new ArrayList<Integer>();
+        idList.add(16);
+        idList.add(17);
+        Integer deleteResult = employeeMapper.deleteBatchIds(idList);
+        System.out.println("deleteResult：" + deleteResult);
+    }
+
+    /**
+     * 通用查询操作
+     */
+    @Test
+    public void testCommonSelect() {
+        // 1、通过id查询
+        Employee employee = employeeMapper.selectById(14);
+        System.out.println(employee);
+
+        // 2、通过多个列查询 id+lastName
+        Employee employee1 = new Employee();
+        employee1.setId(13);
+        employee1.setLastName("White");
+        Wrapper<Employee> employeeWrapper = new QueryWrapper<Employee>(employee1);
+        Employee selectOne = employeeMapper.selectOne(employeeWrapper);
+        System.out.println(selectOne);
+
+        // 3、通过多个id进行查询
+        List<Integer> idList = new ArrayList<Integer>();
+        idList.add(11);
+        idList.add(12);
+        idList.add(13);
+        idList.add(14);
+        List<Employee> employeeList = employeeMapper.selectBatchIds(idList);
+        System.out.println("employeeList：" + employeeList);
+
+        // 4、通过 selectMaps 查询
+        Map<String, Object> columnMap = new HashMap<String, Object>();
+        columnMap.put("last_name", "White");
+        columnMap.put("gender", 0);
+        List<Employee> selectByMap = employeeMapper.selectByMap(columnMap);
+        System.out.println("selectByMap：" + selectByMap);
+
+        // 5、分页查询 selectPage 使用分页查询需要在 applicationContext.xml 中添加 分页查询插件 配置
+        IPage<Employee> employeeIPage = employeeMapper.selectPage(new Page<>(2, 1), null);
+        System.out.println("employeeIPage：" + employeeIPage.getRecords());
+    }
+
+    /**
+     * 通用更新操作
+     */
+    @Test
+    public void testCommonUpdate() {
+        // 初始化employee 对象
+        Employee employee = new Employee();
+        employee.setId(6);
+        employee.setLastName("MP");
+        employee.setEmail("mybatis-plus@github.com");
+        employee.setGender(0);
+//        employee.setAge(33);
+
+        Integer result = employeeMapper.updateById(employee);
+        System.out.println("result：" + result);
+    }
 
     /**
      * 通用 插入操作
@@ -41,7 +129,7 @@ public class TestMp {
 
         // 获取当前数据在数据库中的主键值
         Integer key = employee.getId();
-        System.out.println("key："+key);
+        System.out.println("key：" + key);
     }
 
     @Test

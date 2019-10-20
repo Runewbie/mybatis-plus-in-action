4983f41f4e7f52a4f9af679ae3a2996aa2ce4c51
lijian <0102lijian@gmail.com>
2019-10-20 15:47:00 +0800

    【MP-In-Action】：代码生成器

---
+++ mp05/src/test/java/com/mp/test/TestMp.java
@@ -0,0 +1,16 @@
+package com.mp.test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+/**
+ * @author: li_jian
+ * @version: 1.0
+ * @date: 2019/10/14 21:33
+ * @description：
+ */
+public class TestMp {
+    private ApplicationContext ioc = new
+            ClassPathXmlApplicationContext("applicationContext.xml");
+
+}
